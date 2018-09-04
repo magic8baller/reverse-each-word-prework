424c72c42f1d633aa8d3da747d78d3066d252e73
Leslie Turis <lturis@gmail.com>
2018-09-04 21:52:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
+# both reversed_collect & reversed_each are ARRAYS
+
 def reverse_each_word(sentence)
   sentence = sentence.split(' ')
-  reversed_array << sentence.each {|words| words.reverse}
-  return reversed_array.join(' ')
+  reversed_each << sentence.each { |words| words.reverse }
+  return reversed_each.join(' ')
 end
 
 #note: this didn't pass without the #each without using .join on the return!
@@ -9,3 +11,7 @@ def reverse_each_word(sentence)
   reversed_collect = sentence.split(' ').collect { |words| words.reverse }
   reversed_collect.join(' ')
 end
+
+# => #collect is simpler-- don't need to creeate a new empty array /pushing the calculated values to it like #each!, specifically:
+# the #collect => results in a new array & returns that
+# and #each => just returns the original array(that's why you need to either push or save value in empty array)

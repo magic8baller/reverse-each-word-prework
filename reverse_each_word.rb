317f379c5775e6b1fd55c23df1c26d20d0ec63ca
Leslie Turis <lturis@gmail.com>
2018-09-04 21:53:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,6 +12,9 @@ def reverse_each_word(sentence)
   reversed_collect.join(' ')
 end
 
-# => #collect is simpler-- don't need to creeate a new empty array /pushing the calculated values to it like #each!, specifically:
-# the #collect => results in a new array & returns that
-# and #each => just returns the original array(that's why you need to either push or save value in empty array)
+=begin
+***  #collect is simpler-- don't need to creeate a new empty array /pushing the calculated values to it like #each!, specifically:
+
+#collect => results in a new array & returns that
+and #each => just returns the original array(that's why you need to either push or save value in empty array)
+=end

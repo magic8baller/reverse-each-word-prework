0adc4a92c68dfdf398499d83f36b212578fc6dd1
Leslie Turis <lturis@gmail.com>
2018-09-04 21:42:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(sentence)
   sentence = sentence.split(' ')
-  reversed_array << sentence.each {|words| words.reverse!}
+  reversed_array << sentence.each {|words| words.reverse}
   return reversed_array.join(' ')
 end
 
-
+#note: this didn't pass without the #each without using .join on the return!
 def reverse_each_word(sentence)
   reversed_collect = sentence.split(' ').collect { |words| words.reverse }
   reversed_collect.join(' ')
